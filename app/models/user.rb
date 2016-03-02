615fc7d5d7298219862089ca2b4f77c5f9c8389f
poc.hsu <poc7667@gmail.com>
2016-03-02 15:51:43 +0800

    add queries methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,30 @@
 class User < ActiveRecord::Base
+    has_many :orders
     def self.never_has_any_order
-        query = <<END
+        query = <<SQL_QUERY
             SELECT
             users.id
             FROM users
             LEFT JOIN orders
             ON users.id = orders.user_id
             WHERE orders.id IS NULL
-END
+SQL_QUERY
         ActiveRecord::Base.connection.select_all(query)
     end
+
+    def self.who_has_orders
+        query = <<SQL_QUERY
+            SELECT
+            users.id
+            FROM users
+            LEFT JOIN orders
+            ON users.id = orders.user_id
+            WHERE orders.id IS NOT NULL
+SQL_QUERY
+        ActiveRecord::Base.connection.select_all(query)
+    end
+
+    scope :who_has_orders_using_scope, -> { joins(:orders) }
+    scope :who_has_orders_using_scope_and_outer_join, -> { includes(:orders, :users) }
+    #{ where('published_at > ?', Time.now) }
 end
